f8d556cff7faf9da76cde62a665d2ab0d6b12e95
Matteo0619 <matteo.verita@gmail.com>
2022-12-06 22:17:40 +0100

    Add day6 2022

---
+++ src/main/java/adventofcode/y2022/day6/FirstExercise.java
@@ -0,0 +1,41 @@
+package adventofcode.y2022.day6;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstExercise {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("res/2022/day6/input.csv");
+        FileReader reader = new FileReader(file);
+        BufferedReader buffer = new BufferedReader(reader);
+
+        String line = null;
+        String input = buffer.readLine();
+        int b = 0;
+        int e = 4;
+
+        Set<String> set = new HashSet<String>();
+
+        for(int i = 0; i<input.length()-4; i++) {
+            String[] sub = input.substring(b, e).split("");
+            set.addAll(Arrays.asList(sub));
+
+            if(set.size() == 4) {
+                continue;
+            }
+            b++;
+            e++;
+            set.clear();
+        }
+
+        System.out.println(e);
+
+
+    }
+}
