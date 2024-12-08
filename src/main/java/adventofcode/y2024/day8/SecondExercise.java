141a2c5b694403fef6c547809087cea193105700
matte <matteo.verita@gmail.com>
2024-12-08 14:10:06 +0100

    2024 day8

---
+++ src/main/java/adventofcode/y2024/day8/SecondExercise.java
@@ -0,0 +1,83 @@
+package adventofcode.y2024.day8;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class SecondExercise {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("res/2024/day8/input.csv");
+        FileReader reader = new FileReader(file);
+        BufferedReader buffer = new BufferedReader(reader);
+
+        String line = null;
+
+        List<List<String>> grid = new ArrayList<>();
+        List<List<String>> clone = new ArrayList<>();
+
+        while ((line = buffer.readLine()) != null ) {
+            grid.add(new ArrayList<>(Arrays.asList(line.split(""))));
+            clone.add(new ArrayList<>(Arrays.asList(line.split(""))));
+        }
+
+        Map<String, List<int[]>> map = new HashMap<>();
+
+        for(int i=0; i<grid.size(); i++) {
+            for(int j=0; j<grid.get(i).size(); j++) {
+                if(!grid.get(i).get(j).equals(".")) {
+                    String s = grid.get(i).get(j);
+                    map.putIfAbsent(s, new ArrayList<>());
+                    map.get(s).add(new int[] {i,j});
+                }
+            }
+        }
+
+        for(String k : map.keySet()) {
+            for (int i = 0; i < map.get(k).size(); i++) {
+                for (int j = i + 1; j < map.get(k).size(); j++) {
+                    int[] pos1 = map.get(k).get(i);
+                    int[] pos2 = map.get(k).get(j);
+                    int dX1 = pos1[1] - pos2[1];
+                    int dY1 = pos1[0] - pos2[0];
+                    int dX2 = pos2[1] - pos1[1];
+                    int dY2 = pos2[0] - pos1[0];
+                    for(int c=1; c<Integer.MAX_VALUE; c++) {
+                        int[] posAntinode1= new int[]{pos1[0]+(dY1*c), pos1[1]+(dX1*c)};
+                        try {
+                            clone.get(posAntinode1[0]).set(posAntinode1[1], "#");
+                        } catch (Exception e){
+                            break;
+                        }
+                    }
+                    for(int c=1; c<Integer.MAX_VALUE; c++) {
+                        int[] posAntinode2= new int[]{pos2[0]+(dY2*c), pos2[1]+(dX2*c)};
+                        try {
+                            clone.get(posAntinode2[0]).set(posAntinode2[1], "#");
+                        } catch (Exception e){
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        int counter = 0;
+
+        for (List<String> list : clone) {
+            for (String s : list) {
+                System.out.print(s);
+                if (!s.equals(".")) {
+                    counter++;
+                }
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println(counter);
+
+    }
+
+}
